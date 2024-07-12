ac2c340adcae67cf3c120dfe3b6d26baa65f29f2
Richard-Raph <richardakpan77@gmail.com>
2024-07-12 11:31:09 +0100

    created signup-lecture page

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+    document.querySelector("#signin").style.display = "block";
+    document.querySelector("#signup").style.display = "none";
+}
+
+function register() {
+    document.querySelector("#signup").style.display = "block";
+    document.querySelector("#signin").style.display = "none";
+}
